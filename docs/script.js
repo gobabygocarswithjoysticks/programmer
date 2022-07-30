c743af221727930aadb655ac3c4bc7e9b0e804e2
joshua-8 <joshua_phelps@brown.edu>
2022-07-30 14:59:20 -0400

    code and board options

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,6 @@
+var options = null;
+var configurations_info = null;
+
 document.addEventListener('DOMContentLoaded', async function () {
     // runs on startup
     // check if web serial is enabled
@@ -10,39 +13,75 @@ document.addEventListener('DOMContentLoaded', async function () {
 
 async function updateUpload() {
     var checkBox = document.getElementById("upload-main-checkbox");
-    var configurations_info = await getConfigInfo(checkBox.checked);
+    configurations_info = await getConfigInfo(checkBox.checked);
+    configurations_info = configurations_info.split("\n");
 
     if (configurations_info == null) {
-        document.getElementById("program-selector-alert").innerHTML = "The list of programs available to upload can not be retrieved. Please check your internet connection, try again in 10 minutes and if the problem persists please contact us.";
+        document.getElementById("no-config-alert").innerHTML = "The list of programs available to upload can not be retrieved. Please check your internet connection, try again in 10 minutes and if the problem persists please contact us.";
+        document.getElementById("upload-controls-div").hidden = true;
 
-        updateProgramOptionsSelector(null);
-        document.getElementById("source-name-display").innerHTML = "";
+        document.getElementById("source-name-display").innerHTML = "Error: configurations info is null!";
 
     } else { //there are options for programs to upload
-        document.getElementById("program-selector-alert").innerHTML = "";
+        document.getElementById("no-config-alert").innerHTML = "";
+        document.getElementById("upload-controls-div").hidden = false;
 
-        var optionsRows = configurations_info.split("\n").slice(2, -1); //get just the rows with data for an option
+        options = configurations_info.slice(2, -1); //get just the rows with data for an option
         //split rows and make 2d array
-        for (var i = 0; i < optionsRows.length; i++) {
-            optionsRows[i] = optionsRows[i].split(",");
+        for (var i = 0; i < options.length; i++) {
+            options[i] = options[i].split(", ");
         }
 
+        updateProgramOptionsSelector();
 
-        //uses https://stackoverflow.com/a/42123984 to find unique values
-        var program_options = [... new Set(extractColumn(optionsRows, 1))];
-
-        updateProgramOptionsSelector(program_options);
-        document.getElementById("source-name-display").innerHTML = "source: " + configurations_info.split("\n").slice(1, 2);
+        document.getElementById("source-name-display").innerHTML = "source: " + configurations_info.slice(1, 2);
     }
 }
+function getCode() {
+    var program_selector = document.getElementById("program-selector");
+    var program = program_selector.options[program_selector.selectedIndex].text;
+    var board_selector = document.getElementById("board-selector");
+    var board = board_selector.options[board_selector.selectedIndex].text;
+    console.log("get code", configurations_info[0], configurations_info[1], program, board);
+}
+function updateBoardOptionsSelector() {
+    var program_selector = document.getElementById("program-selector");
+    var selected_program = program_selector.options[program_selector.selectedIndex].text;
+    var board_options = options.filter((v) => { return v[1] === selected_program });
+    board_options = [... new Set(extractColumn(board_options, 2))];
 
-function updateProgramOptionsSelector(program_options) {
+    var select = document.getElementById("board-selector");
+
+    if (board_options == null) {
+        while (select.firstChild) { //empty selector
+            select.removeChild(select.firstChild);
+        }
+    } else {
+        while (select.firstChild) { //empty selector before filling it
+            select.removeChild(select.firstChild);
+        }
+        // https://www.geeksforgeeks.org/how-to-create-a-dropdown-list-with-array-values-using-javascript/
+        for (var i = 0; i < board_options.length; i++) {
+            var optn = board_options[i];
+            var el = document.createElement("option");
+            el.textContent = optn;
+            el.value = optn;
+            select.appendChild(el);
+        }
+    }
+    getCode();
+}
+function updateProgramOptionsSelector() {
+    //uses https://stackoverflow.com/a/42123984 to find unique values
+    var program_options = [... new Set(extractColumn(options, 1))];
+
     var select = document.getElementById("program-selector");
 
     if (program_options == null) {
-        select.hidden = true;
+        while (select.firstChild) { //empty selector
+            select.removeChild(select.firstChild);
+        }
     } else {
-        select.hidden = false;
         while (select.firstChild) { //empty selector before filling it
             select.removeChild(select.firstChild);
         }
@@ -55,6 +94,7 @@ function updateProgramOptionsSelector(program_options) {
             select.appendChild(el);
         }
     }
+    updateBoardOptionsSelector(options);
 }
 
 function extractColumn(arr, column) {
@@ -71,7 +111,6 @@ async function getConfigInfo(fromMain) {
     if (fromMain === true) {
         try {
             result = await getRequest("https://raw.githubusercontent.com/gobabygocarswithjoysticks/car-code/main/hex/configurations-info.txt");
-            console.log(result);
             return "unreleased\n" + "main" + "\n" + result;
         } catch (e) {
             return null;
